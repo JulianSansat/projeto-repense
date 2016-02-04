a1248a33cb47bf34e4fd227d6f27c5ccc99e85d8
Julian Sansat <kerarchie@yahoo.com.ar>
2016-02-04 15:20:11 -0300

    validation of models

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ActiveRecord::Base
 	has_many :classrooms
+	validates :name,:status,:description, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
 	has_many :classrooms
+	validates :name,:status,:register_number, presence: true
 end
