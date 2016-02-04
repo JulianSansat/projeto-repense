998bf112b07a27d1ece66350bf10180af17bb121
Julian Sansat <kerarchie@yahoo.com.ar>
2016-02-04 20:39:52 -0300

    rspec

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,7 +1,7 @@
 class Classroom < ActiveRecord::Base
   belongs_to :student
   belongs_to :course
-  validates :student, :student_id, :course_id, :entry_at, presence: true
+  validates :student, :course, presence: true
 
   def student_name
   	student.try(:name)
